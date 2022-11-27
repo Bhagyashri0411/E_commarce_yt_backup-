5ab07f8395d284d9b820bf1b8fda86ae52bff703
Bhagyashri0411 <bhagyashripawar0411@gmail.com>
2022-11-27 21:32:20 +0530

    done

---
+++ node_code/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer =() =>{
+    return(
+        <div className="footer">
+            <div className="justify-content-center d-flex">
+                <div className="card-name">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b7/MasterCard_Logo.svg" alt="mastercard" />
+                </div>
+
+                <div className="card-name">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg" alt="visa" />
+                </div>
+
+                <div className="card-name">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b7/MasterCard_Logo.svg" alt="mastercard" />
+                </div>
+                <div className="card-name">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/b/b7/MasterCard_Logo.svg" alt="mastercard" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
